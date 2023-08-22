19924af5838da715d31ea6865dc5a083d10338f6
satyam jha <jhasatyam386@gmail.com>
2023-08-22 14:49:05 +0530

    Initial Commit

---
+++ settings/settingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  settingsApp.swift
+//  settings
+//
+//  Created by r84 on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct settingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
